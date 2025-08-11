c91c4709dd833aa3aec7f3299ae8e703f1686928
ali-imtiyazkhan <itzkhantijara@gmail.com>
2025-08-11 19:38:57 +0530

    Time: 3 ms (66.50%) | Memory: 109.5 MB (60.43%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -1,46 +1,29 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+ #include <queue>
+ #include <vector>
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        if (!root) return 0;
-
+        if(!root) return 0;
         queue<TreeNode*> q;
         q.push(root);
-
-        int level = 0;
-        int maxSum = INT_MIN;
-        int ansLevel = 0;
-
-        while (!q.empty()) {
+        int maxlevel = 1;
+        int level=1;
+        int maxsum = root->val;
+        while(!q.empty()){
             int size = q.size();
-            level++;
-            long long sum = 0;
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-
+            int sum=0;
+            for(int i = 0 ;i<size; ++i){
+                TreeNode* node = q.front(); q.pop();
                 sum += node->val;
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
             }
-
-            if (sum > maxSum) {
-                maxSum = sum;
-                ansLevel = level;
+            if(sum>maxsum){
+                maxsum  = sum;
+                maxlevel = level;
             }
+            level++;
         }
-        return ansLevel;
+        return maxlevel;
     }
 };
