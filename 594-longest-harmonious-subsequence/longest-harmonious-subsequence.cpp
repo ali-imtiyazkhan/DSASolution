d55856d67148db8cdee6fce3c2873c782bba696a
ali-imtiyazkhan <itzkhantijara@gmail.com>
2025-08-16 14:14:32 +0530

    Time: 19 ms (61.99%) | Memory: 36.5 MB (65.15%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-        unordered_map<int,int> freq;
-        // count frequency of each number
-        for(int x : nums) {
-            freq[x]++;
-        }
-
-        int ans = 0;
-        // look for pairs where key+1 exists
-        for(auto &p : freq) {
-            int num = p.first;
-            if(freq.count(num+1)) {
-                ans = max(ans, p.second + freq[num+1]);
+        sort(nums.begin(),nums.end()); 
+        int n = nums.size(); 
+        int i = 0; 
+        int ans = 0; 
+        for(int j = 0; j<n; j++){
+            while(nums[j]-nums[i]>1){
+                i++; 
+            }
+            if(nums[j]-nums[i] == 1){
+                ans = max(ans,j-i+1); 
             }
         }
         return ans;
