dfa12a7ba90c9dfb8cbcd3916cee1a70d78617a5
imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-02-11 23:30:20 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (19.45%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-         
-          queue<int> q;
+        
+        int index = 0;
 
-      int n = nums.size();
-
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                q.push(nums[i]);
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                swap(nums[i], nums[index]);
+                index++;
             }
-        }
-
-
-          int i = 0;
-            while (!q.empty()) {
-            nums[i++] = q.front();
-            q.pop();
-              }
-
-        while (i < n) {
-            nums[i++] = 0;
         }
     }
 };
