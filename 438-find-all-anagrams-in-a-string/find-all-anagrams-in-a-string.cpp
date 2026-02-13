f0c2e5ace942c41c83859b520895c6cdcb06c49b
imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-02-13 21:36:36 +0530

    Time: 4 ms (65.14%) | Memory: 11.5 MB (83.98%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -4,12 +4,9 @@ public:
         vector<int> res;
         if(s.size() < p.size()) return res;
 
-        // frequency count of characters in p
         vector<int> pCount(26, 0);
-        // frequency count of current window in s
         vector<int> window(26, 0);
 
-        // fill frequency of p
         for(char c : p) {
             pCount[c - 'a']++;
         }
@@ -17,20 +14,15 @@ public:
         int n = s.size();
         int m = p.size();
 
-        // initialize the first window
         for(int i = 0; i < m; i++) {
             window[s[i] - 'a']++;
         }
-        // if first window is an anagram
+  
         if(window == pCount) res.push_back(0);
 
-        // slide the window
         for(int i = m; i < n; i++) {
-            // remove the character that goes out of the window
             window[s[i - m] - 'a']--;
-            // add the new character
             window[s[i] - 'a']++;
-            // check if current window is an anagram
             if(window == pCount) res.push_back(i - m + 1);
         }
         return res;
