782ea1755167899ce80e9ce8c8172bd1e088693f
imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-03-03 10:46:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (87.55%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -6,20 +6,17 @@ public:
 
         for (int i = 0; i <= n; i++) {
             
-            // When we reach space or end of string
             if (i == n || s[i] == ' ') {
                 
                 int left = start;
                 int right = i - 1;
 
-                // Reverse current word
                 while (left < right) {
                     swap(s[left], s[right]);
                     left++;
                     right--;
                 }
 
-                // Move start to next word
                 start = i + 1;
             }
         }
