2c5ba12b560202ccd0d36efeb7fbf729f686d1b1
imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-04-26 10:33:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (99.34%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -4,14 +4,14 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> tails; // tails[i] = min possible tail for LIS length i+1
+        vector<int> tails;
         for (int num : nums) {
-            // find first position >= num
+        
             auto it = lower_bound(tails.begin(), tails.end(), num);
             if (it == tails.end()) {
-                tails.push_back(num); // num is bigger than all tails
+                tails.push_back(num); 
             } else {
-                *it = num; // replace to maintain minimal tail
+                *it = num;
             }
         }
         return tails.size();
